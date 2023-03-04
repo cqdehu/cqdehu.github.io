971c2937aaae54976bf29415a95dc54dd6bb4ec1
Lehotaiabel <cqde.hu@gmail.com>
2023-03-04 14:12:49 +0100

    Update

--- main.js
+++ main.js
@@ -7,19 +7,4 @@ var loader = document.querySelector('#preloader')
 window.addEventListener('load',function(){
     loader.style.display = "none"
 })
-
-
-
-settingsBtn.addEventListener("click",testf)
-
-
-Ne_nyomogasd = 0
-
-function testf() {
-Ne_nyomogasd+=1
-if(Ne_nyomogasd==3){
-    alert("Ne nyomogasd!")
-    Ne_nyomogasd=0
-}
-}
     
